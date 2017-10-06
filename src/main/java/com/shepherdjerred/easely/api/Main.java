51c17f9bf169c9909274fd61d4612dd4c3d56f91
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-10-06 10:42:39 -0500

    Enable CORS

--- src/main/java/com/shepherdjerred/easely/api/Main.java
+++ src/main/java/com/shepherdjerred/easely/api/Main.java
@@ -14,6 +14,8 @@ import lombok.extern.log4j.Log4j2;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import static spark.Spark.before;
+import static spark.Spark.options;
 import static spark.Spark.port;
 
 @Log4j2
@@ -21,7 +23,7 @@ public class Main {
 
     private static Store store;
     private static Provider provider;
-    
+
     public static void main(String args[]) {
         setupMysqlStorage();
         setupProvider();
@@ -72,9 +74,26 @@ public class Main {
         new UserController(store).setupRoutes();
     }
 
+    private static void enableCors() {
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "https://easely-web.shepherdjerred.com");
+        });
+    }
+
     /**
      * Returns the port the application should listen on. It will first look for an environment variable named PORT,
      * otherwise it will return 8080
+     *
      * @return
      */
     private static int getPort() {
