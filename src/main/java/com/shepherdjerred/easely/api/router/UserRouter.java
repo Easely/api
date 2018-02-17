75aa96f558092f2ff61f1098e1a6de1c2db1d4e3
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-02-16 22:31:55 -0600

    Use EaselyConfig object for values

--- src/main/java/com/shepherdjerred/easely/api/router/UserRouter.java
+++ src/main/java/com/shepherdjerred/easely/api/router/UserRouter.java
@@ -4,12 +4,13 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shepherdjerred.easely.api.config.EaselyConfig;
 import com.shepherdjerred.easely.api.controller.UserController;
 import com.shepherdjerred.easely.api.controller.payload.LoginPayload;
 import com.shepherdjerred.easely.api.controller.payload.PostLoginPayload;
 import com.shepherdjerred.easely.api.controller.payload.RegisterPayload;
 import com.shepherdjerred.easely.api.model.User;
-import com.shepherdjerred.easely.api.storage.Store;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 import javax.security.auth.login.FailedLoginException;
@@ -18,13 +19,12 @@ import java.io.UnsupportedEncodingException;
 import static spark.Spark.post;
 
 @Log4j2
+@AllArgsConstructor
 public class UserRouter implements Router {
+
     private static ObjectMapper objectMapper = new ObjectMapper();
     private UserController userController;
-
-    public UserRouter(Store store) {
-        userController = new UserController(store);
-    }
+    private EaselyConfig easelyConfig;
 
     @Override
     public void setupRoutes() {
@@ -51,10 +51,9 @@ public class UserRouter implements Router {
             }
 
             try {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                Algorithm algorithm = Algorithm.HMAC256(processBuilder.environment().get("JWT_SECRET"));
+                Algorithm algorithm = Algorithm.HMAC256(easelyConfig.getJwtSecret());
                 String token = JWT.create()
-                        .withIssuer("http://easely.shepherdjerred.com")
+                        .withIssuer(easelyConfig.getJwtIssuer())
                         .withClaim("email", loginPayload.getEmail())
                         .withClaim("uuid", String.valueOf(user.getUuid()))
                         .withClaim("easelUsername", user.getEaselUsername())
@@ -79,10 +78,9 @@ public class UserRouter implements Router {
             User user = userController.register(registerPayload.getEmail(), registerPayload.getPassword(), registerPayload.getEaselUsername(), registerPayload.getEaselPassword());
 
             try {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                Algorithm algorithm = Algorithm.HMAC256(processBuilder.environment().get("JWT_SECRET"));
+                Algorithm algorithm = Algorithm.HMAC256(easelyConfig.getJwtSecret());
                 String token = JWT.create()
-                        .withIssuer("https://easely.shepherdjerred.com")
+                        .withIssuer(easelyConfig.getJwtIssuer())
                         .withClaim("email", registerPayload.getEmail())
                         .withClaim("uuid", String.valueOf(user.getUuid()))
                         .withClaim("easelUsername", user.getEaselUsername())
--- src/main/java/com/shepherdjerred/easely/api/router/filters/AuthenticationFilter.java
+++ src/main/java/com/shepherdjerred/easely/api/router/filters/AuthenticationFilter.java
@@ -4,8 +4,10 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.shepherdjerred.easely.api.config.EaselyConfig;
 import com.shepherdjerred.easely.api.model.User;
 import com.shepherdjerred.easely.api.storage.Store;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import spark.Filter;
 import spark.Request;
@@ -18,25 +20,19 @@ import java.util.UUID;
 import static spark.Spark.halt;
 
 @Log4j2
+@AllArgsConstructor
 public class AuthenticationFilter implements Filter {
 
     private Store store;
-    private String jwtSecret;
-
-    public AuthenticationFilter(Store store) {
-        this.store = store;
-        // TODO make this an env var
-        jwtSecret = new ProcessBuilder().environment().get("JWT_SECRET");
-    }
+    private EaselyConfig easelyConfig;
 
     @Override
     public void handle(Request request, Response response) {
         log.debug("Attempting to validate request");
         DecodedJWT jwt;
         try {
-            Algorithm algorithm = Algorithm.HMAC256(jwtSecret);
-            // TODO issuer should be an env var
-            String issuer = "http://easely.shepherdjerred.com";
+            Algorithm algorithm = Algorithm.HMAC256(easelyConfig.getJwtSecret());
+            String issuer = easelyConfig.getJwtIssuer();
             JWTVerifier verifier = com.auth0.jwt.JWT.require(algorithm)
                     .withIssuer(issuer)
                     .build();
