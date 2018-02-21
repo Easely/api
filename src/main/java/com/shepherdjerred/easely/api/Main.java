b7f105a5207586023facd48ef7a443fdd558eb4b
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-02-21 13:25:05 -0600

    Use old redison

--- src/main/java/com/shepherdjerred/easely/api/Main.java
+++ src/main/java/com/shepherdjerred/easely/api/Main.java
@@ -4,7 +4,7 @@ import com.shepherdjerred.easely.api.config.EaselyConfig;
 import com.shepherdjerred.easely.api.config.EnvVarEaselyConfig;
 import com.shepherdjerred.easely.api.easel.adapter.EaselAdapter;
 import com.shepherdjerred.easely.api.easel.adapter.ScraperEaselAdapter;
-import com.shepherdjerred.easely.api.easel.scraper.LiveEaselScraper;
+import com.shepherdjerred.easely.api.easel.scraper.CachedEaselScraper;
 import com.shepherdjerred.easely.api.http.router.AssignmentRouter;
 import com.shepherdjerred.easely.api.http.router.CourseRouter;
 import com.shepherdjerred.easely.api.http.router.UserRouter;
@@ -24,7 +24,7 @@ public class Main {
     public static void main(String args[]) {
         easelyConfig = new EnvVarEaselyConfig();
         setupMysqlStore();
-        easelAdapter = new ScraperEaselAdapter(new LiveEaselScraper());
+        easelAdapter = new ScraperEaselAdapter(new CachedEaselScraper(easelyConfig));
         setupRoutes();
     }
 
